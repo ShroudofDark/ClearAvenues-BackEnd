ef0aad85a34aecc98a259ead95d49eca06d13236
Matthew Wilson <mwils031@odu.edu>
2023-02-02 18:57:32 -0500

    Add user creation and password change functions + change class name

--- src/database/main.java
+++ src/database/main.java
@@ -5,14 +5,17 @@ import java.util.ArrayList;
 public class main {
 
 	public static void main(String[] args) {
-		DatabaseActions dbConnection = new DatabaseActions();
+		UserDatabaseActions dbConnection = new UserDatabaseActions();
 		/* ArrayList<String> emails = dbConnection.getEmails();
 		System.out.println("Emails:");
 		for(String email : emails) {
 			System.out.println("\t" + email);
 		}*/
 		
-		boolean authorized = dbConnection.isAuthorized("fm");
-		System.out.println(authorized);
+		//boolean authorized = dbConnection.isAuthorized("fm");
+		//System.out.println(authorized);
+		
+		//dbConnection.createNewUser("mwils031@odu.edu_duplicate", "Matthew Wilson", "password1", "maintainer");
+		dbConnection.resetPassword("mwils031@odu.edu", "password1", "test1");
 	}
 }
