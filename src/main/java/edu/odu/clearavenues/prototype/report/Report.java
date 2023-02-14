28a499b1e6f681a359f92e41df5516b11a0cbaea
Matthew Wilson <mwils031@odu.edu>
2023-02-13 19:38:19 -0500

    Add functionality to Report and ReportController to create new report

--- src/main/java/edu/odu/clearavenues/prototype/report/Report.java
+++ src/main/java/edu/odu/clearavenues/prototype/report/Report.java
@@ -1,19 +1,91 @@
 package edu.odu.clearavenues.prototype.report;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
 
 
 @Entity
+@Table(name = "reports")
 public class Report {
     public enum Type {
-        SIGN,
-        ACCIDENT
+        DEBRIS,
+        VEHICLE_ACCIDENT,
+        VANDALISM,
+        MISSING_SIGNAGE,
+        POTHOLE,
+        OTHER
+    }
+
+    public enum Status {
+        SUBMITTED,
+        IN_PROGRESS,
+        CLOSED
     }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private int reportId;
 
     @Enumerated(EnumType.STRING)
-    private Type type;
+    private Type reportType;
 
+    private int reportLocationLat;
+
+    private int reportLocationLong;
+
+    private String submittedBy;
+
+    private String reportComment;
+
+    @CreationTimestamp
+    private LocalDateTime reportDate;
+
+    private int reportScore;
+
+    private LocalDateTime resolutionDate;
+
+    private String resolvedBy;
+
+    private int locationId;
+
+    @Enumerated(EnumType.STRING)
+    private Status reportStatus;
+
+    public Report(Type reportType, int latitude, int longitude, String email, String comment, int locationId){
+        this.reportType = reportType;
+        this.reportLocationLat = latitude;
+        this.reportLocationLong = longitude;
+        this.submittedBy = email;
+        this.reportComment = comment;
+        this.locationId = locationId;
+        this.reportStatus = Status.SUBMITTED;
+    }
+
+    public int getReportId() {return reportId;}
+
+    public int getReportLocationLatitude() {return reportLocationLat;}
+
+    public int getReportLocationLongitude() {return reportLocationLong;}
+
+    public String getSubmittedBy() {return submittedBy;}
+
+    public String getReportComment() {return reportComment;}
+
+    public LocalDateTime getReportDate() {return reportDate;}
+
+    public int getReportScore() {return reportScore;}
+
+    public LocalDateTime getResolutionDate() {return resolutionDate;}
+
+    public String getResolvedBy() {return resolvedBy;}
+
+    public int getLocationId() {return locationId;}
+
+    public void setReportComment(String comment) {this.reportComment = comment;}
+
+    public void setResolutionDate(LocalDateTime date) {this.resolutionDate = date;}
+
+    public void setResolvedBy(String email) {this.resolvedBy = email;}
+
+    public void setReportStatus(Status status) {this.reportStatus = status;}
 }
--- src/main/java/edu/odu/clearavenues/prototype/report/ReportController.java
+++ src/main/java/edu/odu/clearavenues/prototype/report/ReportController.java
@@ -1,20 +1,33 @@
 package edu.odu.clearavenues.prototype.report;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Controller
 public class ReportController {
+    @Autowired
     private ReportRepository reportRepository;
 
     @GetMapping("/reports")
     @ResponseBody
     public Optional<Report> getReportById(@RequestParam int id){
         return reportRepository.findById(id);
+    }
+
+    @GetMapping("/createReport")
+    @ResponseBody
+    public void createReport(@RequestParam("reportType") String reportType, @RequestParam("latitude") int latitude, @RequestParam("longitude") int longitude,
+                             @RequestParam("submittedBy") String submittedBy, @RequestParam("comment") String comment, @RequestParam("locationId") int locationId) {
+
+        Report.Type type = Report.Type.valueOf(reportType.toUpperCase());
+        Report report = new Report(type, latitude, longitude, submittedBy, comment, locationId);
+        reportRepository.save(report);
     }
 
     /* Ignore this for now
