4a1a41f03ca1c3abab2df86713d25dd5ced5fb8b
mwils031 <112352366+mwils031@users.noreply.github.com>
2023-04-03 21:38:24 -0400

    Delete src/main/java/edu/odu/clearavenues/prototype/locations directory

--- src/main/java/edu/odu/clearavenues/prototype/locations/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.odu.clearavenues.prototype.locations;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface LocationRepository extends CrudRepository<Location, Integer> {
-
-    Location findByLocationId(int id);
-}
